d556890a0dbfb7dee597ff56e5dfebb1561934a5
Lucas Cherkewski <lucas@ecustom.ca>
2016-07-09 10:37:50 -0400

    Update our reference to Datepicker function, w/ noConflict mode

--- assets/js/open-table-widget.js
+++ assets/js/open-table-widget.js
@@ -15,12 +15,48 @@ jQuery(document).ready(function ($) {
 	$('.otw-party-size-select').on('change', function () {
 		$('.PartySize').val($(this).val());
 	});
-	
+
+	var now = (Date.now() - 86400000); //allow today to be selected
+
 	//Datepicker Initialization
-	$(".otw-reservation-date").otwdatepicker({
-		autoClose: true,
-		dateFormat: "mm/dd/yyyy",
-		weekStart: 0
+	$(".otw-reservation-date").datepicker({
+		autohide : true,
+		format: "mm/dd/yyyy",
+		weekStart: 0,
+		filter: function ( date ) {
+			return date.valueOf() >= now;
+		},
+		template: (
+			'<div class="open-table-widget-datepicker datepicker-container">' +
+			'<div class="datepicker-panel" data-view="years picker">' +
+			'<ul>' +
+			'<li data-view="years prev">&lsaquo;</li>' +
+			'<li data-view="years current"></li>' +
+			'<li data-view="years next">&rsaquo;</li>' +
+			'</ul>' +
+			'<ul data-view="years"></ul>' +
+			'</div>' +
+			'<div class="datepicker-panel" data-view="months picker">' +
+			'<ul>' +
+			'<li data-view="year prev">&lsaquo;</li>' +
+			'<li data-view="year current"></li>' +
+			'<li data-view="year next">&rsaquo;</li>' +
+			'</ul>' +
+			'<ul data-view="months"></ul>' +
+			'</div>' +
+			'<div class="datepicker-panel" data-view="days picker">' +
+			'<ul>' +
+			'<li data-view="month prev">&lsaquo;</li>' +
+			'<li data-view="month current"></li>' +
+			'<li data-view="month next">&rsaquo;</li>' +
+			'</ul>' +
+			'<ul data-view="week"></ul>' +
+			'<ul data-view="days"></ul>' +
+			'</div>' +
+			'</div>'
+		)
 	});
+
+	$.fn.datepicker.noConflict();
 
 });
